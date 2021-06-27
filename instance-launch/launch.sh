0d17bd978147726ad63f6bacb9c45b1a318f438f
saisindhu96 <saisindhuraju@gmail.com>
2021-06-27 11:28:45 +0530

    added

--- instance-launch/launch.sh
+++ instance-launch/launch.sh
@@ -15,7 +15,7 @@ LVER=1
 DNS_UPDATE() {
   PRIVATEIP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z09197723I40S5UZUMAJ5 --change-batch file:///tmp/record.json | jq
+  aws route53 change-resource-record-sets --hosted-zone-id Z09197723I40S5UZUMAJ5  --change-batch file:///tmp/record.json | jq
 }
 
 INSTANCE_CREATE(){
